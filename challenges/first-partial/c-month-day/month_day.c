abde3549847bae93d9fbe5e2d80bd9409a6cabbf
a01634819 <almaanguiano52@gmail.com>
2021-09-09 03:52:10 +0000

    Cambio3

--- challenges/first-partial/c-month-day/month_day.c
+++ challenges/first-partial/c-month-day/month_day.c
@@ -1,6 +1,9 @@
 /* Alma Anguiano A01634819*/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 //Initialization
 static char daytab[2][13] = {
@@ -8,49 +11,55 @@ static char daytab[2][13] = {
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
 
-int day_of_year(int year,int month,int day);
-void month_day(int year,int yearday);
-
-/* Get year day from a given year,month and day combination */
-int day_of_year(int year, int month, int day){
-    int i, leap;
-    leap = year%4 == 0 && year%100 != 0 ¦¦ year%400 == 0;
-
-    for (i = 1; i < month; i++)
-        day += daytab[leap][i];
-    return day;
-}
-
-/* month_day function's prototype
-void month_day(int year, int yearday, int *pmonth, int *pday){
-
-}*/
-
-void month_day(int year,int yearday){
-    int i,leap;
-
-    leap = year % 4 == 0 && year%100 != 0 || year%400 == 0;
-
-    for(i=1;yearday > daytab[leap][i];i++)
-        yearday -= daytab[leap][i];
-
-    
-}
-
-/* month_name:  return name of n-th month */
-char *month_name(int n) {
-
-   static char *name[] = {
+static char *name[] = {
        "Illegal month",
        "January", "February", "March",
        "April", "May", "June",
        "July", "August", "September",
        "October", "November", "December"
-   };
-   return (n < 1 ¦¦ n > 12) ? name[0] : name[n];
+};
+
+int day_of_year(int year,int month,int day);
+//void month_day(int year,int yearday);
+
+/* Get year day from a given year,month and day combination */
+/*int day_of_year(int year, int month, int day){
+    int i, leap;
+    //leap = year%4 == 0 && year%100 != 0 ¦¦ year%400 == 0;
+
+    for (i = 1; i < month; i++)
+        day += daytab[leap][i];
+    return day;
+}*/
+
+// month_day function's prototype
+void month_day(int year, int yearday, int *pmonth, int *pday){
+    int i,leap;
+
+    leap = year % 4 == 0 && year%100 != 0 || year%400 == 0;
+
+    if((yearday < 1) || (leap == 0 && yearday > 365) || (leap == 1 && yearday > 366) ){
+        printf("Error\n");
+        exit(0); 
+    }
+
+    for (int i = 1; i < 12; i++){
+        *pmonth = i;
+        if (*pday <= daytab[leap][i]){
+            return; 
+        }
+        *pday -= daytab[leap][i];
+    }
+
 }
 
-
-int main(void) {
+int main(int argc, char **argv) {
+    
+    int x=0;
+    int y = atoi(argv[1]);
+    int d = atoi(argv[2]);
+    
+    month_day(y, d, &x, &d);
+    printf("%s %02d, %d\n",name[x], d, y);
 
 }
