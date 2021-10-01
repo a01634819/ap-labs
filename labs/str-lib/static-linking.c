b3e4f66fbef28232e39659396bdfea17f392994b
a01634819 <almaanguiano52@gmail.com>
2021-10-01 03:39:40 +0000

    test

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,33 +1,5 @@
 #include <stdio.h>
-#include <string.h>
 
-extern int mystrlen(char *);
-extern char *mystradd(char *, char *);
-extern int mystrfind(char *, char *);
-
-int main(int argc, char *argv[]) {
-    
-    
-    if (argc == 4 && strcmp(argv[1], "-add") == 0) {
-        printf("Initial Lenght: %d\n", mystrlen(argv[2]));
-        char *new_string = mystradd(argv[2],argv[3]);
-        printf("New String    : %s\n", new_string);
-        printf("New length    : %d\n", mystrlen(new_string));
-        return 0;
-    }
-    
-    
-    if (argc == 4 && strcmp(argv[1], "-find") == 0) {
-        int find = mystrfind(argv[2], argv[3]);
-        if (find > -1) {
-            printf("['%s'] string was found at [%d] position\n", argv[3], find);
-        } else {
-            printf("Error");
-        }
-        return 0;
-    }else {
-        printf("Error \n");
-    }
-
+int main() {
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,33 +1,15 @@
-int mystrlen(char *str) {
-    if (*str == '\0')
-        return 0;
-    
-    return (1 + mystrlen(++str));
+lines (9 sloc)  166 Bytes
+   
+
+
+int mystrlen(char *str){
+    return 0;
 }
 
 char *mystradd(char *origin, char *addition){
-    char *str = origin + mystrlen(origin);
-    while (*addition != '\0'){
-        *str++ = *addition++;
-    }
-    *str = '\0';
-    return origin;
+    return 0;
 }
 
 int mystrfind(char *origin, char *substr){
-    int x = mystrlen(substr);
-    int a = 0, b = 0,c = 0;
-    while (origin[a] != '\0') {
-        c = a;
-        while (origin[a] == substr[b] && origin[a] != '\0') {      
-            b++;
-            a++;           
-        }
-        if (b == x) {
-            return c;
-        }
-        b = 0;
-        a++;    
-    }
     return 0;
 }
