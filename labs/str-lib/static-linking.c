dc10c34ad692aeff068e08828838877047d1aafc
a01634819 <almaanguiano52@gmail.com>
2021-10-01 03:33:58 +0000

    String Manipulation Library

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+extern int mystrlen(char *);
+extern char *mystradd(char *, char *);
+extern int mystrfind(char *, char *);
+
+int main(int argc, char *argv[]) {
+    
+    
+    if (argc == 4 && strcmp(argv[1], "-add") == 0) {
+        printf("Initial Lenght: %d\n", mystrlen(argv[2]));
+        char *new_string = mystradd(argv[2],argv[3]);
+        printf("New String    : %s\n", new_string);
+        printf("New length    : %d\n", mystrlen(new_string));
+        return 0;
+    }
+    
+    
+    if (argc == 4 && strcmp(argv[1], "-find") == 0) {
+        int find = mystrfind(argv[2], argv[3]);
+        if (find > -1) {
+            printf("['%s'] string was found at [%d] position\n", argv[3], find);
+        } else {
+            printf("Error");
+        }
+        return 0;
+    }else {
+        printf("Error \n");
+    }
+
     return 0;
 }
