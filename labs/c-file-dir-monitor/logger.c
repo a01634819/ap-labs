b0e6ea6504531ab652df3afccfdf17ebb0b4e015
a01634819 <almaanguiano52@gmail.com>
2021-11-11 05:59:12 +0000

    monitor

--- labs/c-file-dir-monitor/logger.c
+++ labs/c-file-dir-monitor/logger.c
@@ -1,23 +1,83 @@
 #include <stdio.h>
 #include "logger.h"
+#include <time.h>
+#include <syslog.h>
+#include <stdarg.h>
+#include <string.h>
+
+int log=0;
 
 int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
+    printf("Logger starting  %s\n", logType);
+
+    if (strcmp(logType,"stdout") == 0){
+        log = 0;
+    } else if (strcmp(logType,"syslog") == 0){
+            log = 1;
+    }else{       
+        printf("Error");
+        return -1;
+    }   
     return 0;
 }
 
-int infof(const char *format, ...) {
+int infof ( const char * format, ... ){
+    va_list args;
+    va_start (args, format);
+
+    if(log == 1){
+        openlog("Logger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_EMERG,format, args);
+        closelog();
+    } else {
+        vprintf (format, args);
+    }
+    va_end (args);
+    return 1;
+}
+
+int warnf ( const char * format, ... ){
+    va_list args;
+    va_start (args, format);
+
+    if(log == 1){
+        openlog("Logger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_EMERG,format, args);
+        closelog();
+    } else {
+        vprintf (format, args);
+    }
+    va_end (args);
     return 0;
 }
 
-int warnf(const char *format, ...) {
+int errorf ( const char * format, ... ){
+    va_list arg;
+    va_start (arg, format);
+    if(log == 1){
+        openlog("Logger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_EMERG,format, arg);
+        closelog();
+
+    } else {
+        vprintf (format, arg);
+    }
+    va_end (arg);
     return 0;
 }
 
-int errorf(const char *format, ...) {
-    return 0;
-}
-int panicf(const char *format, ...) {
-    return 0;
+int panicf ( const char * format, ... ){
+    va_list arg;
+    va_start (arg, format);
+
+    if(log == 1){
+        openlog("Logger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_EMERG,format, arg);
+        closelog();
+    } else {
+        vprintf (format, arg);
+    }
+    va_end (arg);
+    return 1;
 }
 
--- labs/c-file-dir-monitor/monitor.c
+++ labs/c-file-dir-monitor/monitor.c
@@ -1,7 +1,61 @@
 #include <stdio.h>
 #include "logger.h"
+#include <unistd.h>
+#include <sys/inotify.h>
+#include <ftw.h>
 
-int main(){
-    // Place your magic here
+struct inotify_event *iNotifyEvent;
+int main(int argc, char** argv){
+    char *size;
+    int iNotifyAPI;
+	int readd;
+    int x = 1;
+    char buffer[1024 * (sizeof(struct inotify_event) + 16)];
+	int wind;
+	
+	if(argc == 2) {
+		iNotifyAPI = inotify_init();
+		if(iNotifyAPI == -1)
+			errorf("Error");
+		wind = inotify_add_watch(iNotifyAPI, argv[1], IN_ALL_EVENTS);
+		if(wind == -1)
+			errorf("Error");
+        infof("Starting file/directory monitor on %s", argv[1]);
+		while(x == 1) {
+			readd = read(iNotifyAPI, buffer, 1024*(sizeof(struct inotify_event)+16));
+			for (size=buffer; size<buffer+readd;){
+				iNotifyEvent = (struct inotify_event*) size;
+                if (iNotifyEvent->mask & IN_CREATE){
+					if(iNotifyEvent->mask & IN_ISDIR){
+                    	printf("[Directory - ");
+                	}else{
+                    	printf("[File - ");
+                	}
+		            infof("Create] - %s", iNotifyEvent->name);
+				}
+	            if (iNotifyEvent->mask & IN_DELETE){
+					if(iNotifyEvent->mask & IN_ISDIR){
+                    	printf("[Directory - ");
+                	}else{
+                    	printf("[File - ");
+                	}
+		            infof("Removal] - %s", iNotifyEvent->name);
+				}
+	            if (iNotifyEvent->mask & IN_MOVE){
+					if(iNotifyEvent->mask & IN_ISDIR){
+                    	printf("[Directory - ");
+                	}else{
+                    	printf("[File - ");
+                	}
+		            infof("Rename] - %s", iNotifyEvent->name);
+				}
+				watchSize += sizeof(struct inotify_event)+iNotifyEvent->len;
+			}
+		}
+		inotify_rm_watch(iNotifyAPI, wind);
+		close(iNotifyAPI);
+	}
+	else
+		warnf("Error");
     return 0;
 }
